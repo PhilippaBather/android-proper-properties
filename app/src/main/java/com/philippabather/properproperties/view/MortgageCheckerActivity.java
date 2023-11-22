01266d00e8646732d44fe355e31ffda2543e070c
pabather <p.a.bather1@gmail.com>
2023-11-22 13:59:07 +0100

    created home; added mortgage activity (placeholder)

---
+++ app/src/main/java/com/philippabather/properproperties/view/MortgageCheckerActivity.java
@@ -0,0 +1,16 @@
+package com.philippabather.properproperties.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.philippabather.properproperties.R;
+
+public class MortgageCheckerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mortgage_checker);
+    }
+}
