1ed86f39691b3aefd8545ffa8143d63dcb569967
pabather <p.a.bather1@gmail.com>
2023-11-26 18:26:17 +0100

    added mortgage checker class

--- app/src/main/java/com/philippabather/properproperties/domain/MortgageChecker.java
+++ app/src/main/java/com/philippabather/properproperties/domain/MortgageChecker.java
@@ -5,38 +5,42 @@ import java.math.RoundingMode;
 import java.text.NumberFormat;
 import java.util.Locale;
 
-public class MortgageQuoteCalculator {
+/**
+ * MortgageChecker - calcula el pago mensual de una hipoteca.
+ *
+ * @author Philippa Bather
+ */
+public class MortgageChecker {
 
     private static final NumberFormat moneyFormatter = NumberFormat.getCurrencyInstance(Locale.FRANCE);
-    final int MONTHS_IN_YEAR = 12;
-    private double price;
-    private double deposit;
-    private double rate;
-    private int duration;
-    private String postcode;
+    public static final int MONTHS_IN_YEAR = 12;
 
-    public MortgageQuoteCalculator(double price, double deposit, double rate, int duration, String postcode) {
-        this.price = price;
-        this.deposit = deposit;
-        this.rate = rate;
-        this.duration = duration;
-        this.postcode = postcode;
-    }
-
-    public String calculate() {
-        BigDecimal loan = BigDecimal.valueOf(price).subtract(BigDecimal.valueOf(deposit));
-        BigDecimal monthlyRate = BigDecimal.valueOf(rate).divide(BigDecimal.valueOf(MONTHS_IN_YEAR), 5, RoundingMode.HALF_UP);
-        BigDecimal monthlyRatePlusOne = monthlyRate.add(BigDecimal.ONE);
-        int monthlyPayments = duration * MONTHS_IN_YEAR;
+    /**
+     * Calcula el pago mensual de una hipoteca usando la fórmula:
+     *
+     * H = P * (t(1 + t)^n / (1 + t)^n - 1)
+     *
+     * H = hipoteca, P = préstamo, t = tasa de interés, n = número de pagos mensuales
+     *
+     * @param price - precio del inmueble
+     * @param deposit - déposito
+     * @param rate - tasa de interés (%)
+     * @param duration - duración del préstamo
+     * @return pago mensual formatado en euros
+     */
+    public static String calculate(double price, double deposit, double rate, int duration) {
+        BigDecimal loan = BigDecimal.valueOf(price).subtract(BigDecimal.valueOf(deposit));  // préstamo
+        BigDecimal monthlyRate = BigDecimal.valueOf(rate).divide(BigDecimal.valueOf(MONTHS_IN_YEAR), 5, RoundingMode.HALF_UP);  // tasa mensual
+        BigDecimal monthlyRatePlusOne = monthlyRate.add(BigDecimal.ONE);  // tasa mensual más 1
+        int monthlyPayments = duration * MONTHS_IN_YEAR; // número de pagos mensuales
 
         BigDecimal dividend = monthlyRatePlusOne.pow(monthlyPayments).subtract(BigDecimal.ONE);
         dividend = monthlyRate.multiply(dividend);
         BigDecimal divisor = monthlyRatePlusOne.pow(monthlyPayments).subtract(BigDecimal.ONE);
-        BigDecimal quotient = dividend.divide(divisor, 5, RoundingMode.HALF_UP);
+        BigDecimal quotient = dividend.divide(divisor, 5, RoundingMode.HALF_UP);  // cociente
 
-        BigDecimal repayment = loan.multiply(quotient);
+        BigDecimal monthlyRepayment = loan.multiply(quotient);  // reembolso mensual
 
-        return moneyFormatter.format(repayment).toString() + " p/m";
+        return moneyFormatter.format(monthlyRepayment).toString() + " p/m";
     }
-
 }
