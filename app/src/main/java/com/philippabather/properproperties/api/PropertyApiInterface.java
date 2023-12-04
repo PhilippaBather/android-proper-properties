9986cf8889e9431b858e6c731ab30f6aa135db1c
pabather <p.a.bather1@gmail.com>
2023-12-04 09:23:57 +0100

    added initial functionality for DB

--- app/src/main/java/com/philippabather/properproperties/api/PropertyApiInterface.java
+++ app/src/main/java/com/philippabather/properproperties/api/PropertyApiInterface.java
@@ -1,2 +1,16 @@
-package com.philippabather.properproperties.api;public interface PropertyApiInterface {
+package com.philippabather.properproperties.api;
+
+import com.philippabather.properproperties.domain.Property;
+
+import java.util.Set;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Headers;
+
+public interface PropertyApiInterface {
+
+    @Headers({"Accept: application/json"})
+    @GET("/properties")
+    Call<Set<Property>> getProperties();
 }
