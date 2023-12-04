af17087c4ed1b426270172df3f06e30d487eb175
pabather <p.a.bather1@gmail.com>
2023-12-04 09:22:03 +0100

    added presenter

--- app/src/main/java/com/philippabather/properproperties/presenter/PropertyListPresenter.java
+++ app/src/main/java/com/philippabather/properproperties/presenter/PropertyListPresenter.java
@@ -1,6 +1,34 @@
 package com.philippabather.properproperties.presenter;
 
-public class PropertyListPresenter {
+import com.philippabather.properproperties.contract.PropertyListContract;
+import com.philippabather.properproperties.domain.Property;
+import com.philippabather.properproperties.model.PropertyListModel;
+import com.philippabather.properproperties.view.PropertyListView;
 
-    // TODO
+import java.util.List;
+
+public class PropertyListPresenter implements PropertyListContract.Presenter, PropertyListContract.Model.OnLoadPropertiesListener {
+
+    private final PropertyListModel model;
+    private final PropertyListView view;
+
+    public PropertyListPresenter(PropertyListView view) {
+        this.view = view;
+        this.model = new PropertyListModel();
+    }
+
+    @Override
+    public void loadAllProperties() {
+        model.loadAllProperties(this);
+    }
+
+    @Override
+    public void onLoadPropertiesSuccess(List<Property> properties) {
+        view.listProperties(properties);
+    }
+
+    @Override
+    public void onLoadPropertiesError(String msg) {
+        view.showMessage(msg);
+    }
 }
