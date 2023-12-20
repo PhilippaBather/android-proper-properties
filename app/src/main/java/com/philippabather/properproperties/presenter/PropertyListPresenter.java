aaa2b02246e6406bc4cbb8f34eb6fe5d5495bb53
pabather <p.a.bather1@gmail.com>
2023-12-20 22:25:50 +0100

    updated for favourites

--- app/src/main/java/com/philippabather/properproperties/presenter/PropertyListPresenter.java
+++ app/src/main/java/com/philippabather/properproperties/presenter/PropertyListPresenter.java
@@ -1,21 +1,25 @@
 package com.philippabather.properproperties.presenter;
 
 import com.philippabather.properproperties.contract.PropertyListContract;
+import com.philippabather.properproperties.domain.RentalFavourite;
 import com.philippabather.properproperties.domain.RentalProperty;
+import com.philippabather.properproperties.domain.SaleFavourite;
 import com.philippabather.properproperties.domain.SaleProperty;
 import com.philippabather.properproperties.model.PropertyListModel;
 import com.philippabather.properproperties.view.PropertyListView;
 
 import java.util.List;
 
-public class PropertyListPresenter implements PropertyListContract.Presenter, PropertyListContract.Model.OnLoadPropertiesListener {
+public class PropertyListPresenter implements PropertyListContract.Presenter,
+        PropertyListContract.Model.OnLoadPropertiesListener,
+        PropertyListContract.Model.OnLoadFavouritesListener {
 
     private final PropertyListModel model;
     private final PropertyListView view;
 
     public PropertyListPresenter(PropertyListView view) {
         this.view = view;
-        this.model = new PropertyListModel();
+        this.model = new PropertyListModel(view);
     }
 
     @Override
@@ -40,6 +44,31 @@ public class PropertyListPresenter implements PropertyListContract.Presenter, Pr
 
     @Override
     public void onLoadPropertiesError(String msg) {
+        view.showMessage(msg);
+    }
+
+    @Override
+    public void loadFavouriteRentals() {
+        model.loadRentalFavourites(this);
+    }
+
+    @Override
+    public void loadFavouriteSales() {
+        model.loadSaleFavourites(this);
+    }
+
+    @Override
+    public void onLoadRentalFavouritesSuccess(List<RentalFavourite> favourites) {
+        view.listFavouriteRentals(favourites);
+    }
+
+    @Override
+    public void onLoadSaleFavouritesSuccess(List<SaleFavourite> favourites) {
+        view.listFavouriteSales(favourites);
+    }
+
+    @Override
+    public void onLoadFavouritesError(String msg) {
         view.showMessage(msg);
     }
 }
