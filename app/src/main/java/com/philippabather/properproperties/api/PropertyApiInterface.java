2ffe26911b340febdb7b8f815c736e263c79e61a
pabather <p.a.bather1@gmail.com>
2023-12-20 02:21:01 +0100

    corrected get sale by id path

--- app/src/main/java/com/philippabather/properproperties/api/PropertyApiInterface.java
+++ app/src/main/java/com/philippabather/properproperties/api/PropertyApiInterface.java
@@ -8,14 +8,23 @@ import java.util.Set;
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Headers;
+import retrofit2.http.Path;
 
 public interface PropertyApiInterface {
 
     @Headers({"Accept: application/json"})
     @GET("/properties/rental")
     Call<Set<RentalProperty>> getRentalProperties();
+    @Headers({"Accept: application/json"})
+    @GET("/properties/rental/{rentalId}")
+    Call<RentalProperty> getRentalPropertyById(@Path(value = "rentalId") long rentalId);
 
     @Headers({"Accept: application/json"})
     @GET("/properties/sale")
     Call<Set<SaleProperty>> getSaleProperties();
+
+    @Headers({"Accept: application/json"})
+    @GET("/properties/sale/{saleId}")
+    Call<SaleProperty> getSalePropertyById(@Path(value = "saleId") long saleId);
+
 }
