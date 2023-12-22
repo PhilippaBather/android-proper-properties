8b8e15d9ad329333a7b7f6df79c4b76e2debbd07
pabather <p.a.bather1@gmail.com>
2023-12-22 02:24:53 +0100

    added proprietor class

--- app/src/main/java/com/philippabather/properproperties/domain/Proprietor.java
+++ app/src/main/java/com/philippabather/properproperties/domain/Proprietor.java
@@ -1,2 +1,88 @@
-package com.philippabather.properproperties.domain;public class Proprietor {
+package com.philippabather.properproperties.domain;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class Proprietor {
+    private String username;
+    private String name;
+    private String surname;
+    private String email;
+//    private String accountCreated; // fecha de la creacíón de la cuenta
+//    private LocalDateTime accountCreatedLD;
+    private long id;
+    private int numProperties;
+    private boolean isAgency;
+    private List<RentalProperty> rentalPropertyList;
+    private List<SaleProperty> salePropertyList;
+
+    public Proprietor(String username, String name, String surname, String email,
+                      long id, int numProperties, boolean isAgency,
+                      List<RentalProperty> rentalPropertyList, List<SaleProperty> salePropertyList) {
+        this.username = username;
+        this.name = name;
+        this.surname = surname;
+        this.email = email;
+//        this.accountCreatedLD = accountCreated;
+        this.id = id;
+        this.numProperties = numProperties;
+        this.isAgency = isAgency;
+        this.rentalPropertyList = rentalPropertyList;
+        this.salePropertyList = salePropertyList;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+//    public LocalDateTime getAccountCreated() {
+//        return accountCreated;
+//    }
+
+    public long getId() {
+        return id;
+    }
+
+    public int getNumProperties() {
+        return numProperties;
+    }
+
+    public boolean isAgency() {
+        return isAgency;
+    }
+
+    public List<RentalProperty> getRentalPropertyList() {
+        return rentalPropertyList;
+    }
+
+    public List<SaleProperty> getSalePropertyList() {
+        return salePropertyList;
+    }
+
+    @Override
+    public String toString() {
+        return "Proprietor{" +
+                "username='" + username + '\'' +
+                ", name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", email='" + email + '\'' +
+                ", id=" + id +
+                ", numProperties=" + numProperties +
+                ", isAgency=" + isAgency +
+                ", rentalPropertyList=" + rentalPropertyList +
+                ", salePropertyList=" + salePropertyList +
+                '}';
+    }
 }
