899cffe3a7d0cea4f36862db02546ded85a2fdd2
pabather <p.a.bather1@gmail.com>
2023-12-22 02:25:46 +0100

    refactored

--- app/src/main/java/com/philippabather/properproperties/domain/SaleFavourite.java
+++ app/src/main/java/com/philippabather/properproperties/domain/SaleFavourite.java
@@ -1,2 +1,47 @@
-package com.philippabather.properproperties.domain;public class SaleFav {
+package com.philippabather.properproperties.domain;
+
+import androidx.annotation.NonNull;
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity
+public class SaleFavourite {
+    @PrimaryKey
+    @NonNull
+    private Long id;
+
+    @NonNull
+    @ColumnInfo
+    private Long salePropertyId;
+
+    public SaleFavourite(@NonNull Long salePropertyId) {
+        this.salePropertyId = salePropertyId;
+    }
+
+    @NonNull
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(@NonNull Long id) {
+        this.id = id;
+    }
+
+    @NonNull
+    public Long getSalePropertyId() {
+        return salePropertyId;
+    }
+
+    public void setSalePropertyId(@NonNull Long salePropertyId) {
+        this.salePropertyId = salePropertyId;
+    }
+
+    @Override
+    public String toString() {
+        return "SaleFavourite{" +
+                "id=" + id +
+                ", salePropertyId=" + salePropertyId +
+                '}';
+    }
 }
