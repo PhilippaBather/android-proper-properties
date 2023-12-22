6ca1c5f70de2891674e109eacf2aabf80d5b5ab6
pabather <p.a.bather1@gmail.com>
2023-12-22 02:30:14 +0100

    added fragments

--- app/src/main/java/com/philippabather/properproperties/view/RentalFragment.java
+++ app/src/main/java/com/philippabather/properproperties/view/RentalFragment.java
@@ -1,31 +1,51 @@
 package com.philippabather.properproperties.view;
 
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import com.philippabather.properproperties.R;
+import com.philippabather.properproperties.adapter.RentalPropertyAdapter;
+import com.philippabather.properproperties.domain.RentalProperty;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class RentalFragment extends Fragment {
+    private List<RentalProperty> rentalPropertyList;
+    private RentalPropertyAdapter adapter;
+    private LinearLayoutManager linearLayoutManager;
+    private RecyclerView recyclerView;
 
-    // views created after onCreate
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_rental, container, false);
-    }
+        View view = inflater.inflate(R.layout.fragment_rental, container, false);
 
-    // method used to access views
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
+        // recibe datos desde los argumentos pasados
+        Bundle bundle = getArguments();
+        rentalPropertyList = new ArrayList<>();
+        rentalPropertyList = getArguments().getParcelableArrayList("rentals");
+
+        // crea el RecyclerView
+        recyclerView = view.findViewById(R.id.recyclerview_rental_property_list);
+        recyclerView.setHasFixedSize(true);
+
+        // establece el LinearLayoutManager
+        linearLayoutManager = new LinearLayoutManager(view.getContext());
+        recyclerView.setLayoutManager(linearLayoutManager);
+
+        // establece el Adapter y bindea al RecyclerView
+        adapter = new RentalPropertyAdapter(rentalPropertyList, null);
+        recyclerView.setAdapter(adapter);
+        adapter.notifyDataSetChanged();
+
+        return view;
     }
 }
