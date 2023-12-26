54d99647138bbddbe80298db07b94d24dcba94c6
pabather <p.a.bather1@gmail.com>
2023-12-26 04:13:26 +0100

    added registration contract

--- app/src/main/java/com/philippabather/properproperties/contract/PropertyRegistrationContract.java
+++ app/src/main/java/com/philippabather/properproperties/contract/PropertyRegistrationContract.java
@@ -1,2 +1,27 @@
-package com.philippabather.properproperties.contract;public interface PropertyRegistrationContract {
+package com.philippabather.properproperties.contract;
+
+import com.philippabather.properproperties.domain.RentalProperty;
+import com.philippabather.properproperties.domain.SaleProperty;
+
+public interface PropertyRegistrationContract {
+
+    interface Model {
+        interface OnRegisterPropertyListener {
+            void onRegisterRentalPropertySuccess(RentalProperty rental);
+            void onRegisterSalePropertySuccess(SaleProperty sale);
+            void onRegisterPropertyError(String msg);
+        }
+
+        void createNewRentalProperty(OnRegisterPropertyListener listener, long proprietorId, RentalProperty rental);
+        void createNewSaleProperty(OnRegisterPropertyListener listener, long proprietorId, SaleProperty sale);
+    }
+
+    interface View {
+        void showMessage(String msg);
+    }
+
+    interface Presenter {
+        void createNewRentalProperty(long proprietorId, RentalProperty rental);
+        void createNewSaleProperty(long proprietorId, SaleProperty sale);
+    }
 }
