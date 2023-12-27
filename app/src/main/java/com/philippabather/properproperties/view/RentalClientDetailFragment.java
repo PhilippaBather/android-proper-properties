f8465f4cb483a36ce2b5b52cff0b26588b5f50c4
pabather <p.a.bather1@gmail.com>
2023-12-28 00:25:10 +0100

    added rental detail fragment

--- app/src/main/java/com/philippabather/properproperties/view/RentalClientDetailFragment.java
+++ app/src/main/java/com/philippabather/properproperties/view/RentalClientDetailFragment.java
@@ -1,64 +1,114 @@
-package com.philippabather.properproperties;
+package com.philippabather.properproperties.view;
 
+import static com.philippabather.properproperties.constants.Constants.BUNDLE_ARGUMENT_RENTAL;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.TextView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link RentalClientDetailFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class RentalClientDetailFragment extends Fragment {
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+import com.mapbox.maps.MapView;
+import com.mapbox.maps.Style;
+import com.mapbox.maps.plugin.annotation.generated.PointAnnotationManager;
+import com.philippabather.properproperties.R;
+import com.philippabather.properproperties.domain.RentalProperty;
+import com.philippabather.properproperties.map.MapUtils;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+public class RentalClientDetailFragment extends Fragment implements Style.OnStyleLoaded {
+    private Button btnBack;
+    private CheckBox cbPropertyFurnished;
+    private CheckBox cbPropertyLift;
+    private CheckBox cbPropertyParking;
+    private CheckBox cbPropertyPets;
+    private MapView mapView;
+    private TextView tvPropertyBathrooms;
+    private TextView tvPropertyBedrooms;
+    private TextView tvPropertyDeposit;
+    private TextView tvPropertyDescription;
+    private TextView tvPropertyMinTen;
+    private TextView tvPropertyPrice;
+    private TextView tvPropertySize;
+    private TextView tvPropertyType;
 
-    public RentalClientDetailFragment() {
-        // Required empty public constructor
-    }
+    private RentalProperty rental;
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment RentalClientDetailFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static RentalClientDetailFragment newInstance(String param1, String param2) {
-        RentalClientDetailFragment fragment = new RentalClientDetailFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    private PointAnnotationManager pointAnnotationManager; // MapBox libraries - for annotating the map
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_rental_client_detail, container, false);
+        View view = inflater.inflate(R.layout.fragment_rental_client_detail, container, false);
+
+        assert getArguments() != null;
+        rental = getArguments().getParcelable(BUNDLE_ARGUMENT_RENTAL);
+
+        findViews(view);
+        setViews();
+        setUpMap();
+        setClickListeners();
+
+        return view;
+    }
+
+    private void findViews(View view) {
+        btnBack = view.findViewById(R.id.btn_cancel);
+        cbPropertyFurnished = view.findViewById(R.id.cb_property_leasehold);
+        cbPropertyLift = view.findViewById(R.id.cb_property_lift);
+        cbPropertyParking = view.findViewById(R.id.cb_property_parking);
+        cbPropertyPets = view.findViewById(R.id.cb_property_pets);
+        mapView = view.findViewById(R.id.mapView);
+        tvPropertyBathrooms = view.findViewById(R.id.tv_property_info_bathrooms);
+        tvPropertyBedrooms = view.findViewById(R.id.tv_property_info_bedrooms);
+        tvPropertyDeposit = view.findViewById(R.id.tv_property_info_deposit);
+        tvPropertyDescription = view.findViewById(R.id.tv_property_info_description);
+        tvPropertyMinTen = view.findViewById(R.id.tv_property_info_min_tenancy);
+        tvPropertyPrice = view.findViewById(R.id.tv_property_info_price);
+        tvPropertySize = view.findViewById(R.id.tv_property_info_size);
+        tvPropertyType = view.findViewById(R.id.tv_property_info_type);
+    }
+
+    private void setViews() {
+        cbPropertyFurnished.setChecked(rental.isFurnished());
+        cbPropertyLift.setChecked(rental.isLift());
+        cbPropertyParking.setChecked(rental.isParking());
+        cbPropertyPets.setChecked(rental.isPetFriendly());
+        tvPropertyBathrooms.setText(String.valueOf(rental.getNumBathrooms()));
+        tvPropertyBedrooms.setText(String.valueOf(rental.getNumBedrooms()));
+        tvPropertyDeposit.setText(String.valueOf(rental.getDeposit()));
+        tvPropertyDescription.setText(rental.getDescription());
+        tvPropertyMinTen .setText(String.valueOf(rental.getMinTenancy()));
+        tvPropertyPrice.setText(String.valueOf(rental.getRentPerMonth()));
+        tvPropertySize.setText(String.valueOf(rental.getMetresSqr()));
+        tvPropertyType.setText(String.valueOf(rental.getPropertyType()));
+    }
+
+    private void setUpMap() {
+        mapView.getMapboxMap().loadStyleUri(Style.MAPBOX_STREETS, this);
+        pointAnnotationManager = MapUtils.initializePointAnnotationManager(mapView);
+        Bitmap marker = BitmapFactory.decodeResource(getResources(), R.mipmap.blue_marker_view);
+        MapUtils.addMarker(pointAnnotationManager, marker, rental.getLatitude(), rental.getLongitude());
+    }
+
+    @Override
+    public void onStyleLoaded(@NonNull Style style) {
+        MapUtils.setCameraPositionAndZoom(mapView);
+    }
+
+    private void setClickListeners() {
+        btnBack.setOnClickListener(this::goBackToPropertyListView);
+    }
+
+    private void goBackToPropertyListView(View view) {
+        Intent intent = new Intent(view.getContext(), PropertyListView.class);
+        startActivity(intent);
     }
 }
