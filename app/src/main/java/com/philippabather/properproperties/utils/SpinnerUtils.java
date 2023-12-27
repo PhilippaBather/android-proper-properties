27c00ddae25a4cfab522b6724e36070667eab254
pabather <p.a.bather1@gmail.com>
2023-12-27 02:40:27 +0100

    added spinner utils

---
+++ app/src/main/java/com/philippabather/properproperties/utils/SpinnerUtils.java
@@ -0,0 +1,33 @@
+package com.philippabather.properproperties.utils;
+
+import static com.philippabather.properproperties.constants.Constants.PROPERTY_TYPE_COMMERICAL_EN;
+import static com.philippabather.properproperties.constants.Constants.PROPERTY_TYPE_COMMERICAL_ES;
+import static com.philippabather.properproperties.constants.Constants.PROPERTY_TYPE_FLAT_EN;
+import static com.philippabather.properproperties.constants.Constants.PROPERTY_TYPE_FLAT_ES;
+import static com.philippabather.properproperties.constants.Constants.PROPERTY_TYPE_HOUSE_EN;
+import static com.philippabather.properproperties.constants.Constants.PROPERTY_TYPE_HOUSE_ES;
+
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import com.philippabather.properproperties.domain.PropertyType;
+
+public class SpinnerUtils {
+    public static void setUpSpinner(ArrayAdapter<CharSequence> adapter, Spinner spPropertyType,
+                                    AdapterView.OnItemSelectedListener listener) {
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spPropertyType.setAdapter(adapter);
+        spPropertyType.setOnItemSelectedListener(listener);
+    }
+
+    public static PropertyType setPropertyType(String type) {
+        return switch(type.toUpperCase()) {
+            case PROPERTY_TYPE_COMMERICAL_EN, PROPERTY_TYPE_COMMERICAL_ES -> PropertyType.COMMERCIAL;
+            case PROPERTY_TYPE_FLAT_EN, PROPERTY_TYPE_FLAT_ES -> PropertyType.FLAT;
+            case PROPERTY_TYPE_HOUSE_EN, PROPERTY_TYPE_HOUSE_ES -> PropertyType.HOUSE;
+            default -> null;
+        };
+    }
+
+}
