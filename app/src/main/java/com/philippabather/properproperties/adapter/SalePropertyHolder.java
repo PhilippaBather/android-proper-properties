0feb82bc7361ff8990575cab1a07bf07593cb4ac
pabather <p.a.bather1@gmail.com>
2024-01-17 14:10:10 +0100

    refactored Toast to Snackbar

--- app/src/main/java/com/philippabather/properproperties/adapter/SalePropertyHolder.java
+++ app/src/main/java/com/philippabather/properproperties/adapter/SalePropertyHolder.java
@@ -7,16 +7,18 @@ import static com.philippabather.properproperties.constants.Constants.INTENT_EXT
 import static com.philippabather.properproperties.constants.Constants.INTENT_EXTRA_SALE_ID;
 
 import android.content.Intent;
+import android.os.Build;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.material.snackbar.Snackbar;
 import com.philippabather.properproperties.R;
 import com.philippabather.properproperties.db.AppLocalDB;
 import com.philippabather.properproperties.db.DBHelperMethods;
@@ -44,6 +46,7 @@ public class SalePropertyHolder extends RecyclerView.ViewHolder {
     protected TextView tvPropertyPrice;
     protected TextView tvPropertyOverview; // detalles principales
     protected TextView tvPropertyDescription;
+    private View layoutPropertyItem;
 
     private final AppLocalDB localDB;
 
@@ -63,6 +66,7 @@ public class SalePropertyHolder extends RecyclerView.ViewHolder {
         ibPropertyContact = parentView.findViewById(R.id.img_btn_property_contact);
         ibPropertyFavourite = parentView.findViewById(R.id.img_btn_property_favourite);
         ivPropertyImage = parentView.findViewById(R.id.iv_property_img);
+        layoutPropertyItem = parentView.findViewById(R.id.layout_activity_property_item);
         tvPropertyTitle = parentView.findViewById(R.id.tv_property_detail_title);
         tvPropertyPrice = parentView.findViewById(R.id.tv_property_price);
         tvPropertyOverview = parentView.findViewById(R.id.tv_property_key_details);
@@ -90,7 +94,7 @@ public class SalePropertyHolder extends RecyclerView.ViewHolder {
     }
 
     private void contactProprietor() {
-        Toast.makeText(parentView.getContext(), R.string.toast_contacted_owner, Toast.LENGTH_LONG).show();
+        Snackbar.make(layoutPropertyItem, R.string.ui_snackbar_owner_contacted, Snackbar.LENGTH_LONG).show();
     }
 
     private void addToFavourites(List<SaleProperty> properties) {
